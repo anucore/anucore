74f48797c1c304e215b61f9498590f53d047f051
anucore <39303513+anucore@users.noreply.github.com>
2018-05-26 12:29:05 +0530

    Initial commit
    Files uploaded in repository

---
+++ README.md
@@ -0,0 +1,59 @@
+# The AnuCoin
+
+We Introduce the New Cryptocurrency AnuCoin (An Universal Coin)
+AnuCoin is a decentralized digital currency. Its based on a secure C11 algorithm Especially POW/POS mineable cryptocurrency with Masternodes under the Blockchain technology.
+
+## Main futures:
+
+*	ASIC Resistance
+*	NiceHash Resistance
+*	Nvidia & AMD GPU Mining
+*	Wallet Built-in Block Explorer
+*	Friendly Masternode Setup
+*	Long-Term Development & Support
+
+
+### Base Specs:
+
+*	Algo: 		C11
+*	Blocktime:	60 sec
+*	Maturity: 	144 blocks
+*	Diff retargeting: 	every block
+*	Total supply: 	~999 999 999 after 10+ years
+*	Masternode Rewards: 
+	90% of POS Rewards from start
+	60% of POS Rewards from 1051200 block.
+*	PoS coins maturation: 24 hour
+
+
+#### PoW Rewards:
+
+*	From block 1 to 131399 : 1
+*	From block 131400 to 525599 : 2
+*	From block 525600 to 1051199 : 3
+*	From block 1051200 to 1576799 : 6
+*	From block 1576800 to 2102399 : 9
+*	From block 2102400 to 2627999 :18
+*	From block 2628000 to 3153599 : 27
+*	From block 3153600 to 4204799 : 18
+*	From block 4204800 to 4730399 : 9
+*	From block 4730400 to 5255999 : 6
+*	From block 5256000 : 3
+
+
+##### PoS Rewards:
+
+*	From block 1 to 131399 : 2
+*	From block 131400 to 525599 : 4
+*	From block 525600 to 1051199 : 6
+*	From block 1051200 to 1576799 : 36
+*	From block 1576800 to 2102399 : 27
+*	From block 2102400 to 2627999 : 18
+*	From block 2628000 to 3153599 : 9
+*	From block 3153600 to 4730399 : 6
+*	From block 4730400 : 3
+
+###### License
+
+AnuCoin is released under the terms of the MIT license. See COPYING for more information or see http://opensource.org/licenses/MIT.
+
---
+++ src/chainparams.cpp
@@ -0,0 +1,226 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2014-2015 Dash Developers
+// Copyright (c) 2017-2018 The Bithold developers
+// Copyright (c) 2017-2018 The AnuCoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "assert.h"
+#include "chainparams.h"
+#include "main.h"
+#include "util.h"
+#include "base58.h"
+#include <boost/assign/list_of.hpp>
+
+using namespace boost::assign;
+
+struct SeedSpec6 {
+    uint8_t addr[16];
+    uint16_t port;
+};
+
+#include "chainparamsseeds.h"
+//
+// Main network
+//
+
+// Convert the pnSeeds array into usable address objects.
+static void convertSeeds(std::vector<CAddress> &vSeedsOut, const unsigned int *data, unsigned int count, int port)
+{
+     // It'll only connect to one or two seed nodes because once it connects,
+     // it'll get a pile of addresses with newer timestamps.
+     // Seed nodes are given a random 'last seen time' of between one and two
+     // weeks ago.
+     const int64_t nOneWeek = 7*24*60*60;
+    for (unsigned int k = 0; k < count; ++k)
+    {
+        struct in_addr ip;
+        unsigned int i = data[k], t;
+        
+        // -- convert to big endian
+        t =   (i & 0x000000ff) << 24u
+            | (i & 0x0000ff00) << 8u
+            | (i & 0x00ff0000) >> 8u
+            | (i & 0xff000000) >> 24u;
+        
+        memcpy(&ip, &t, sizeof(ip));
+        
+        CAddress addr(CService(ip, port));
+        addr.nTime = GetTime()-GetRand(nOneWeek)-nOneWeek;
+        vSeedsOut.push_back(addr);
+    }
+}
+
+// Hardcoded seeds.
+static void getHardcodedSeeds(std::vector<CAddress> &vSeedsOut)
+{
+    std::vector<std::string> ips;
+    ips.push_back("nodeus.anucoin.org");
+    ips.push_back("nodeeu.anucoin.org");
+    ips.push_back("nodecn.anucoin.org");
+    ips.push_back("nodeau.anucoin.org");
+    ips.push_back("nodein.anucoin.org");
+	
+    const int64_t oneWeek = 7 * 24 * 60 * 60;
+    for (size_t i = 0; i < ips.size(); ++i)
+    {
+        CAddress addr(CService(ips[i], 15971));
+        addr.nTime = GetTime() - GetRand(oneWeek) - oneWeek;
+        vSeedsOut.push_back(addr);
+    }
+}
+class CMainParams : public CChainParams {
+public:
+    CMainParams() {
+        // The message start string is designed to be unlikely to occur in normal data.
+        // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+        // a large 4-byte int at any alignment.
+        pchMessageStart[0] = 0xf9;
+        pchMessageStart[1] = 0xe5;
+        pchMessageStart[2] = 0xa6;
+        pchMessageStart[3] = 0xb8;
+        vAlertPubKey = ParseHex("0496131e15af76eb1d0e4a656ed3594ef64425850aa52617506177cf245575f0aa11e5b1777cfd8621ea39a7996872a07631ea25b3fdda00df37f5e982fe58850a");
+        nDefaultPort = 36963;
+        nRPCPort = 96369;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 20);
+		
+        const char* pszTimestamp = "AnuCoin-An Universal Coin";
+        std::vector<CTxIn> vin;
+        vin.resize(1);
+        vin[0].scriptSig = CScript() << 0 << CBigNum(42) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+        std::vector<CTxOut> vout;
+        vout.resize(1);
+        vout[0].SetEmpty();
+		
+        CTransaction txNew(1, 1526037573, vin, vout, 0);
+
+        LogPrintf("genesis mainnet transaction:  %s\n", txNew.ToString().c_str());
+
+        genesis.vtx.push_back(txNew);
+
+        genesis.hashPrevBlock = 0;
+        genesis.hashMerkleRoot = genesis.BuildMerkleTree();
+        genesis.nVersion = 1;
+        genesis.nTime    = 1526037573;
+        genesis.nBits    = bnProofOfWorkLimit.GetCompact(); 
+        genesis.nNonce   = 2228193;
+        hashGenesisBlock = genesis.GetHash();
+
+        assert(hashGenesisBlock == uint256("0x8a43609851341bf78afaa8aa60a9cf83cd4a71672a7930c57e58d6b7d9074762"));
+        assert(genesis.hashMerkleRoot == uint256("0x6b0dfc6cd8cb03d8134d0b5bf170d02cef69c124ff9946750ec135a9a46e9dfe"));
+
+        
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,23);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,21);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,95);
+        base58Prefixes[STEALTH_ADDRESS] = std::vector<unsigned char>(1,41);
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x99)(0x75)(0x45)(0xE2).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x99)(0x73)(0x43)(0xE3).convert_to_container<std::vector<unsigned char> >();  
+	
+        vSeeds.push_back(CDNSSeedData("1",  "nodeus.anucoin.org"));
+        vSeeds.push_back(CDNSSeedData("2",  "nodeeu.anucoin.org"));
+        vSeeds.push_back(CDNSSeedData("3",  "nodecn.anucoin.org"));
+        vSeeds.push_back(CDNSSeedData("4",  "nodeau.anucoin.org"));
+        vSeeds.push_back(CDNSSeedData("5",  "nodein.anucoin.org"));
+		
+		convertSeeds(vFixedSeeds, pnSeed, ARRAYLEN(pnSeed), nDefaultPort); 
+		
+        getHardcodedSeeds(vFixedSeeds);		
+
+        nPoolMaxTransactions = 3;
+        strDarksendPoolDummyAddress = "AFoQDUrp63QWqFhjEr3Fmc4ubHRhyzjKUC";
+
+        nLastPOWBlock = 150000000;
+        nPOSStartBlock = 1;
+    }
+
+    virtual const CBlock& GenesisBlock() const { return genesis; }
+    virtual Network NetworkID() const { return CChainParams::MAIN; }
+
+    virtual const vector<CAddress>& FixedSeeds() const {
+        return vFixedSeeds;
+    }
+protected:
+    CBlock genesis;
+    vector<CAddress> vFixedSeeds;
+};
+static CMainParams mainParams;
+
+
+//
+// Testnet
+//
+
+class CTestNetParams : public CMainParams {
+public:
+    CTestNetParams() {
+        // The message start string is designed to be unlikely to occur in normal data.
+        // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+        // a large 4-byte int at any alignment.
+        pchMessageStart[0] = 0xf9;
+        pchMessageStart[1] = 0xe5;
+        pchMessageStart[2] = 0xa6;
+        pchMessageStart[3] = 0xb8;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 16);
+        vAlertPubKey = ParseHex("0496131e15af76eb1d0e4a656ed3594ef64425850aa52617506177cf245575f0aa11e5b1777cfd8621ea39a7996872a07631ea25b3fdda00df37f5e982fe58850a");
+        nDefaultPort = 27170;
+        nRPCPort = 27171;
+        strDataDir = "testnet";
+        genesis.nTime    = 1526037573;
+        genesis.nBits    = bnProofOfWorkLimit.GetCompact(); 
+        genesis.nNonce   = 2228193;
+
+        hashGenesisBlock = genesis.GetHash();
+        //assert(hashGenesisBlock == uint256("0x8a43609851341bf78afaa8aa60a9cf83cd4a71672a7930c57e58d6b7d9074762"));
+
+        vFixedSeeds.clear();
+        vSeeds.clear();
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,102); // h
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,23);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,25);
+        base58Prefixes[STEALTH_ADDRESS] = std::vector<unsigned char>(1,43);
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x98)(0x74)(0x44)(0xE1).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x98)(0x72)(0x42)(0xE2).convert_to_container<std::vector<unsigned char> >();
+
+        convertSeeds(vFixedSeeds, pnTestnetSeed, ARRAYLEN(pnTestnetSeed), nDefaultPort);
+
+        nLastPOWBlock = 400000;
+    }
+    virtual Network NetworkID() const { return CChainParams::TESTNET; }
+};
+static CTestNetParams testNetParams;
+
+
+static CChainParams *pCurrentParams = &mainParams;
+
+const CChainParams &Params() {
+    return *pCurrentParams;
+}
+
+void SelectParams(CChainParams::Network network) {
+    switch (network) {
+        case CChainParams::MAIN:
+            pCurrentParams = &mainParams;
+            break;
+        case CChainParams::TESTNET:
+            pCurrentParams = &testNetParams;
+            break;
+        default:
+            assert(false && "Unimplemented network");
+            return;
+    }
+}
+
+bool SelectParamsFromCommandLine() {
+    
+    bool fTestNet = GetBoolArg("-testnet", false);
+    
+    if (fTestNet) {
+        SelectParams(CChainParams::TESTNET);
+    } else {
+        SelectParams(CChainParams::MAIN);
+    }
+    return true;
+}
